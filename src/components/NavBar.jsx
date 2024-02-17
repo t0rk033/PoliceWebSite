8988146175f975c3370e680330d33ee0d92d47cd
t0rk033 <thiagocisse42@gmail.com>
2024-02-17 16:54:59 -0300

    foram ajustados alguns detalhes para dispositivos mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,21 +16,25 @@ function NavBar() {
  const [clasOff, setClassOf] = useState('logoutBtn off')
    useEffect(()=>{
      const unsubscribe = onAuthStateChanged(auth, (user)=>{
-      const email = user.email
-       if(user){
-         setChangeUserName(user.email)
-         setProfile('/profile')
-         setClassOf("")
-       }
-       else{
-         setChangeUserName('Acesse')
-         setProfile('/login')
-       }
-       if (email === 'guimaraes@police.com'){
-        setIncidentReportForAgents('/IncidentReportForAgents')
-       }else{
-        setIncidentReportForAgents('/IncidentReport')
-       }
+      if (user) {
+    // Se o usuário existe e não é nulo
+    const email = user.email; // Aqui ocorre o erro se user for null
+    if (email) {
+      // Verificar se email não é null ou undefined
+      setChangeUserName(user.email);
+      setProfile('/profile');
+      setClassOf('');
+      if (email === 'guimaraes@police.com') {
+        setIncidentReportForAgents('/IncidentReportForAgents');
+      } else {
+        setIncidentReportForAgents('/IncidentReport');
+      }
+    }
+  } else {
+    setChangeUserName('Acesse');
+    setProfile('/login');
+    setClassOf('logoutBtn off');
+  }
 
      })
      return ()=> unsubscribe()
@@ -66,16 +70,16 @@ function NavBar() {
         <ul>
             <li><a href="#">Recursos</a></li>
             <li><a href="#">Sobre nós</a></li>
-            <li><Link to={incidentReportForAgents}>Boletim Online</Link></li>
-            <li><Link to="/sos" href="#">Emergência</Link></li>
+            <li><Link onClick={changeClass} to={incidentReportForAgents}>Boletim Online</Link></li>
+            <li><Link to="/sos" href="#" onClick={changeClass}>Emergência</Link></li>
         </ul>
       </div>
       <div className="rightNavBar">
       <ul>
-            <li><a href="#">Home</a></li>
+            <li><Link onClick={changeClass} to='/'>Home</Link></li>
             <li><a href="#">Carreiras</a></li>
             <li><a href="#">Noticias/Comunicados</a></li>
-            <li><Link to={profile}><i class='bx bxs-user'></i> {changeUserName} </Link> <button onClick={handleLogout} className={clasOff}>Sair</button></li>
+            <li><Link onClick={changeClass} to={profile}><i class='bx bxs-user'></i> {changeUserName} </Link> <button onClick={handleLogout} className={clasOff}>Sair</button></li>
         </ul>
       </div>
       <i className='bx bx-menu' onClick={changeClass}></i>
